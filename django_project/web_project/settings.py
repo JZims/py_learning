6e3215428a264755f81e4fb38ffe5267ba0095c1
JZims <zimmerman.j@hotmail.com>
2023-10-03 02:22:50 -0400

    django templates and static files

--- django_project/web_project/settings.py
+++ django_project/web_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dbx^csd-e2%&#7@vfo)fy4dx2j3=z34qt*$n26%!e8op5!9ns@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
