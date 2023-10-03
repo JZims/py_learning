9c50d3491d25d94abdbddade97257b7576eb11ec
JZims <zimmerman.j@hotmail.com>
2023-10-03 03:00:11 -0400

    It turns blue...

--- django_project/web_project/settings.py
+++ django_project/web_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dbx^csd-e2%&#7@vfo)fy4dx2j3=z34qt*$n26%!e8op5!9ns@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+
+
+mimetypes.add_type("text/css", ".css", True)
 
 
 # Application definition
